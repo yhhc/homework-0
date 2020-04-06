3988afa849d0306d9d59a57fa36bbedb496aa0ef
Henry Chen <chenyh@mit.edu>
2020-04-05 21:34:11 -0400

    Ch03-2nd-Basic data wrangling

--- code.R
+++ code.R
@@ -1,3 +1,11 @@
 # The first line of code
 library(dslabs)
 data(murders)
+library(dplyr)
+
+murders <- mutate(murders,rate=total/population*100000)
+filter(murders,rate<=0.71)
+new_table <- select(murders,state,region,rate)
+filter(new_table,rate<=0.71)
+
+murders %>% select(state,region,rate) %>% filter(rate <=0.71)
