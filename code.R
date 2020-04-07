8f8daeeacafc3401c256ff22c9f96ece846cd20e
Henry Chen <chenyh@mit.edu>
2020-04-06 23:19:59 -0400

    The 3rd video of Ch3: Creating a data frame and  revising the default stringAsFactors value.

--- code.R
+++ code.R
@@ -9,3 +9,9 @@ new_table <- select(murders,state,region,rate)
 filter(new_table,rate<=0.71)
 
 murders %>% select(state,region,rate) %>% filter(rate <=0.71)
+
+# The 3rd videos of Ch3: Creating a data frame
+grades <- data.frame(names=c("John","Juan","Jean","Yao"),
+                     exam_1=c(95,80,90,85),
+                     exam_2=c(90,85,85,90),
+                     stringsAsFactors = FALSE)
